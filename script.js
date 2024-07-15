4a51003a2dbdd3dfffd1eb0b10b088d76ce71b8c
SandeepDinodia <151106767+SandeepDinodia@users.noreply.github.com>
2024-07-15 17:07:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+document.addEventListener("DOMContentLoaded", function() {
+    const searchDiv = document.querySelector('.search');
+    const input = document.querySelector('.input');
+    const button = document.querySelector('.btn');
+
+    button.addEventListener('click', function() {
+        searchDiv.classList.add('active');
+        input.focus();
+    });
+});
